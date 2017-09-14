5a012c797ea2b98d50e60795757d4d923f486acc
Nathaniel Villegas <nathanielvillegas@calvisitor-10-105-164-147.calvisitor.1918.berkeley.edu>
2017-09-13 18:21:57 -0700

    added dynamic port

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+var port = process.env.PORT || 3000;
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
@@ -152,6 +153,4 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+app.listen(port);
